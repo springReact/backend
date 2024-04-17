d939c7013d3aa3dbeaaaea0205ed2e0b3c68c4fe
kingaser <kingaser@naver.com>
2024-04-17 14:48:32 +0900

    QueryDsl Setting

--- src/main/java/org/zerock/apiserver/repository/TodoRepository.java
+++ src/main/java/org/zerock/apiserver/repository/TodoRepository.java
@@ -2,6 +2,7 @@ package org.zerock.apiserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.apiserver.domain.Todo;
+import org.zerock.apiserver.repository.search.TodoSearch;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoSearch {
 }
---
+++ src/main/java/org/zerock/apiserver/repository/search/TodoSearch.java
@@ -0,0 +1,8 @@
+package org.zerock.apiserver.repository.search;
+
+import org.springframework.data.domain.Page;
+import org.zerock.apiserver.domain.Todo;
+
+public interface TodoSearch {
+    Page<Todo> search1();
+}
