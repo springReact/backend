8c4781b4873917d47d88f4f7678efa86d89f6b70
kingaser <kingaser@naver.com>
2024-04-18 15:11:12 +0900

    PageResponseDTO paging 처리 방법 적용

--- src/main/java/org/zerock/apiserver/dto/PageRequestDTO.java
+++ src/main/java/org/zerock/apiserver/dto/PageRequestDTO.java
@@ -1,9 +1,17 @@
 package org.zerock.apiserver.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import static lombok.AccessLevel.PROTECTED;
+
 @SuperBuilder
+@Data
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
 public class PageRequestDTO {
 
     @Builder.Default
