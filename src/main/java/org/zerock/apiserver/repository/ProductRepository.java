42bfc0943e18da5e58b7571056e31594cb868529
kingaser <kingaser@naver.com>
2024-04-24 14:09:48 +0900

    query dsl pageing

--- src/main/java/org/zerock/apiserver/repository/ProductRepository.java
+++ src/main/java/org/zerock/apiserver/repository/ProductRepository.java
@@ -1,15 +1,18 @@
 package org.zerock.apiserver.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.zerock.apiserver.domain.Product;
+import org.zerock.apiserver.repository.search.ProductSearch;
 
 import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductSearch {
 
     @EntityGraph(attributePaths = "imageList")
     @Query("select p from Product p where p.pno = :pno")
@@ -18,4 +21,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Modifying
     @Query("update Product p set p.delFlag = :delFlag where p.pno = :pno")
     void updateToDelete(@Param("pno") Long pno, @Param("delFlag") boolean delFlag);
+
+    @Query("select p, pi from Product p left join p.imageList pi where pi.ord = 0 and p.delFlag = false")
+    Page<Object[]> selectList(Pageable pageable);
 }
--- src/test/java/org/zerock/apiserver/repository/ProductRepositoryTest.java
+++ src/test/java/org/zerock/apiserver/repository/ProductRepositoryTest.java
@@ -2,14 +2,21 @@ package org.zerock.apiserver.repository;
 
 import lombok.extern.log4j.Log4j2;
 import org.assertj.core.api.Assertions;
+import org.hibernate.query.sqm.mutation.internal.cte.CteInsertStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.apiserver.domain.Product;
+import org.zerock.apiserver.dto.PageRequestDTO;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -25,16 +32,19 @@ class ProductRepositoryTest {
     @DisplayName("상품 추가")
     @Test
     void testInsert() {
-        Product product = Product.builder()
-                .pname("Test ")
-                .pdesc("Test Desc")
-                .price(1000)
-                .build();
 
-        product.addImageString(UUID.randomUUID() + "_" + "IMAGE1.jpg");
-        product.addImageString(UUID.randomUUID() + "_" + "IMAGE2.jpg");
+        for (int i = 0; i < 20; i++) {
+            Product product = Product.builder()
+                    .pname("Test " + i)
+                    .pdesc("Test Desc " + i)
+                    .price(1000)
+                    .build();
 
-        productRepository.save(product);
+            product.addImageString(UUID.randomUUID() + "_" + "IMAGE" + i + 1 + ".jpg");
+            product.addImageString(UUID.randomUUID() + "_" + "IMAGE" + i + 11 + ".jpg");
+
+            productRepository.save(product);
+        }
     }
 
     @DisplayName("상품 조회 n + 1")
@@ -89,5 +99,23 @@ class ProductRepositoryTest {
         product.addImageString(UUID.randomUUID() + "_" + "PIMAGE3.jpg");
 
         productRepository.save(product);
+    }
+
+    @DisplayName("상품 리스트 조회 페이징 n + 1")
+    @Test
+    void testList() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("pno").descending());
+
+        Page<Object[]> result = productRepository.selectList(pageable);
+
+        result.getContent().forEach(arr -> log.info(Arrays.toString(arr)));
+    }
+
+    @DisplayName("검색 ")
+    @Test
+    void testSearch() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().build();
+
+        productRepository.searchList(pageRequestDTO);
     }
 }
