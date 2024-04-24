b405342dae442f6902353e371560391b3d074f02
kingaser <kingaser@naver.com>
2024-04-24 13:50:37 +0900

    상품 수정, 삭제

--- src/main/java/org/zerock/apiserver/repository/ProductRepository.java
+++ src/main/java/org/zerock/apiserver/repository/ProductRepository.java
@@ -1,7 +1,21 @@
 package org.zerock.apiserver.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.apiserver.domain.Product;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @EntityGraph(attributePaths = "imageList")
+    @Query("select p from Product p where p.pno = :pno")
+    Optional<Product> selectOne(@Param("pno") Long pno);
+
+    @Modifying
+    @Query("update Product p set p.delFlag = :delFlag where p.pno = :pno")
+    void updateToDelete(@Param("pno") Long pno, @Param("delFlag") boolean delFlag);
 }
--- src/test/java/org/zerock/apiserver/repository/ProductRepositoryTest.java
+++ src/test/java/org/zerock/apiserver/repository/ProductRepositoryTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.apiserver.domain.Product;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,6 +33,60 @@ class ProductRepositoryTest {
 
         product.addImageString(UUID.randomUUID() + "_" + "IMAGE1.jpg");
         product.addImageString(UUID.randomUUID() + "_" + "IMAGE2.jpg");
+
+        productRepository.save(product);
+    }
+
+    @DisplayName("상품 조회 n + 1")
+    @Test
+    void testRead() {
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.findById(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+
+        log.info(product.getImageList());
+    }
+
+    @DisplayName("상품 조회 n + 1(2)")
+    @Test
+    void testRead2() {
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.selectOne(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+
+        log.info(product.getImageList());
+    }
+
+    @Commit
+    @Transactional
+    @DisplayName("상품 삭제")
+    @Test
+    void testDelete() {
+        Long pno = 2L;
+
+        productRepository.updateToDelete(pno, true);
+    }
+
+    @DisplayName("상품 수정")
+    @Test
+    void testUpdate() {
+        Product product = productRepository.selectOne(1L).get();
+
+        product.changePrice(3000);
+
+        product.clearList();
+
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE1.jpg");
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE2.jpg");
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE3.jpg");
 
         productRepository.save(product);
     }
